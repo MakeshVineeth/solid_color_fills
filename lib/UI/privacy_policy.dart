b8cea0d257089065c2947ae285dd2b6cfa7faf3a
Makesh Vineeth <makeshvineeth1997@gmail.com>
2021-01-11 22:14:39 +0530

    improve privacy policy

--- lib/UI/privacy_policy.dart
+++ lib/UI/privacy_policy.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 
 class PrivacyPolicy extends StatelessWidget {
   final text =
-      'We do not collect your personal data, period. Moreover, Solid Color Fills is an Open Source Project. At MakeshTech Hub, accessible from https://makeshvineeth.github.io, one of our main priorities is the privacy of our users. This Privacy Policy document contains types of information that is collected and recorded by MakeshTech Hub and how we use it.';
+      'We do not collect your personal data, period. Moreover, Solid Color Fills is an Open Source Project.';
+
+  final text0 =
+      'At MakeshTech, one of our main priorities is the privacy of our users. This Privacy Policy document contains types of information that is collected and recorded by MakeshTech and how we use it.';
 
   final text1 =
       'If you have additional questions or require more information about our Privacy Policy, do not hesitate to contact us.';
@@ -16,18 +19,20 @@ class PrivacyPolicy extends StatelessWidget {
 
   final h4 = 'Log Files';
   final text4 =
-      'MakeshTech Hub follows a standard procedure of using log files. These are not linked to any information that is personally identifiable. The information collected by log files include Email provided by you during Bug Reports, System OS Version, Screenshots of Crash/Bugs and Device Name/Brand';
+      'MakeshTech follows a standard procedure of using log files. These are not linked to any information that is personally identifiable. The log files are only collected when the user manually generates a bug report. The information collected by log files include email provided by you during Bug Reports, System OS Version, Screenshots of Crash/Bugs and Device Name/Brand.';
 
   final h5 = 'How we use your information';
   final text5 =
-      'This non-critical data will only be used for fixing the reported bugs and will NEVER be shared with anyone or any third-party services. Occasionally, we might ask you for your email for any bug report that you have submitted. This will only be used for contacting you (with your permission) and discuss about that aforementioned bug report, nothing else.';
+      'This non-critical data will only be used for fixing the reported bugs and will never be shared with anyone or any third-party services. Occasionally, we might ask you for your email for any bug report that you have submitted. This will only be used for contacting you (with your permission) and discuss about that aforementioned bug report, nothing else.';
 
   final h6 = 'Advertising Partners Privacy Policies';
   final text6 =
-      'MakeshTech Hub\'s Privacy Policy does not apply to other advertisers. Thus, we are advising you to consult the respective Privacy Policies of these third-party ad servers for more detailed information.';
+      'MakeshTech\'s Privacy Policy does not apply to other advertisers. Thus, we are advising you to consult the respective Privacy Policies of these third-party ad servers for more detailed information.';
 
-  final text7 =
-      'If any of our apps are supported by Ads, be assured that we will also provide an option to disable them through a much affordable in-app purchase. We would also provide an option to disable Interest-Based Ads, thus less tracking, for the people who do not intend to pay.';
+  final text7 = 'So, if any of our apps are supported by advertising, be assured that we will also provide an option to disable them through a much affordable in-app purchase. We would also provide an option to disable Interest-Based Ads, thus less tracking, for the people who do not intend to pay.';
+
+
+  final space = 10.0;
 
   @override
   Widget build(BuildContext context) {
@@ -52,21 +57,76 @@ class PrivacyPolicy extends StatelessWidget {
                     text,
                     style: TextStyle(fontWeight: FontWeight.w600),
                   ),
-                  SizedBox(height: 10),
+                  SizedBox(height: space),
+                  Text(
+                    text0,
+                    style: TextStyle(fontWeight: FontWeight.w600),
+                  ),
+                  SizedBox(height: space),
                   Text(
                     text1,
                     style: TextStyle(fontWeight: FontWeight.w600),
                   ),
+                  SizedBox(height: space),
+                  Text(
+                    h2,
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 18.0,
+                    ),
+                  ),
+                  SizedBox(height: space),
                   Text(
                     text2,
                     style: TextStyle(fontWeight: FontWeight.w600),
                   ),
+                  SizedBox(height: space),
                   Text(
                     text3,
                     style: TextStyle(fontWeight: FontWeight.w600),
                   ),
+                  SizedBox(height: space),
+                  Text(
+                    h4,
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 18.0,
+                    ),
+                  ),
+                  SizedBox(height: space),
                   Text(
                     text4,
+                    style: TextStyle(fontWeight: FontWeight.w600),
+                  ),
+                  SizedBox(height: space),
+                  Text(
+                    h5,
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 18.0,
+                    ),
+                  ),
+                  SizedBox(height: space),
+                  Text(
+                    text5,
+                    style: TextStyle(fontWeight: FontWeight.w600),
+                  ),
+                  SizedBox(height: space),
+                  Text(
+                    h6,
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 18.0,
+                    ),
+                  ),
+                  SizedBox(height: space),
+                  Text(
+                    text6,
+                    style: TextStyle(fontWeight: FontWeight.w600),
+                  ),
+                  SizedBox(height: space),
+                  Text(
+                    text7,
                     style: TextStyle(fontWeight: FontWeight.w600),
                   ),
                 ],
