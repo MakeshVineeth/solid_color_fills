3ddfc79270e9b7e9230cfdc459c37832fbd6d4c7
Makesh Vineeth <makeshvineeth1997@gmail.com>
2021-02-01 14:38:11 +0530

    Set width to 250 for now, set height to x/1.7

--- lib/UI/wall_image.dart
+++ lib/UI/wall_image.dart
@@ -16,23 +16,30 @@ class WallImage extends ConsumerWidget {
     AsyncValue<Uint8List> imageObject = watch(imageProvision);
 
     return AnimatedCrossFade(
-      duration: const Duration(milliseconds: 800),
-      crossFadeState: imageObject.when(
-        data: (data) => CrossFadeState.showFirst,
-        loading: () => CrossFadeState.showSecond,
-        error: (obj, err) => CrossFadeState.showSecond,
-      ),
-      firstChild: containerCustom(
-          child: Image.memory(imageObject?.data?.value ?? kTransparentImage)),
-      secondChild: containerCustom(
-          child: Center(child: const CircularProgressIndicator())),
-    );
+        duration: const Duration(milliseconds: 800),
+        crossFadeState: imageObject.when(
+          data: (data) => CrossFadeState.showFirst,
+          loading: () => CrossFadeState.showSecond,
+          error: (obj, err) => CrossFadeState.showSecond,
+        ),
+        firstChild: containerCustom(
+          child: Image.memory(
+            imageObject?.data?.value ?? kTransparentImage,
+            fit: BoxFit.fill,
+          ),
+        ),
+        secondChild: containerCustom(
+          child: Center(
+            child: const CircularProgressIndicator(),
+          ),
+        ));
   }
 
   Widget containerCustom({@required Widget child}) {
     return Container(
       padding: EdgeInsets.all(8),
-      height: constraints.maxHeight / 1.5,
+      width: 250,
+      height: constraints.maxHeight / 1.7,
       child: ClipRRect(
         borderRadius: fixedValues.fixedCardRadius,
         child: child,
