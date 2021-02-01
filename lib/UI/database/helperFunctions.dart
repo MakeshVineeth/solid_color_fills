0202c305cdcfc64b814db69997f3527bd247cf31
Makesh Vineeth <makeshvineeth1997@gmail.com>
2021-02-01 14:07:23 +0530

    Improve size related operations for walls.

--- lib/UI/database/helperFunctions.dart
+++ lib/UI/database/helperFunctions.dart
@@ -3,14 +3,18 @@ import 'package:solid_color_fills/UI/database/commons.dart';
 import 'package:flutter_riverpod/all.dart';
 import 'package:flutter/material.dart';
 import 'package:solid_color_fills/UI/wall_chooser.dart';
+import 'dart:ui';
 
 void openWallChooser(BuildContext context) {
-  Size size = MediaQuery.of(context).size;
-  final ScreenSize newSize =
-      ScreenSize(size.width.round(), size.height.round());
-  final currentState = context.read(screenSize);
+  Size size = window.physicalSize;
 
-  if (currentState.state.width != newSize.width) currentState.state = newSize;
+  if (size != Size.zero) {
+    final ScreenSize newSize =
+        ScreenSize(size.width.ceil(), size.height.ceil());
+    final currentState = context.read(screenSize);
+
+    if (currentState.state.width != newSize.width) currentState.state = newSize;
+  }
 
   final color = context.read(commonProvider.state).color;
   if (color != null && color != Colors.transparent)
