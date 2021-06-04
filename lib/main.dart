3baec69ed44b3b2b512ce8cde526c1a6440c680e
Makesh Vineeth <makeshvineeth1997@gmail.com>
2021-06-04 13:27:53 +0530

    Use defaultThemeId instead of onInitCallback

--- lib/main.dart
+++ lib/main.dart
@@ -14,23 +14,11 @@ class MaterialHome extends StatelessWidget {
   Widget build(BuildContext context) {
     return ThemeProvider(
       saveThemesOnChange: true,
-      loadThemeOnInit: false,
-      onInitCallback: (controller, savedThemeFuture) async {
-        String savedTheme = await savedThemeFuture;
-
-        if (savedTheme != null) {
-          controller.setTheme(savedTheme);
-        } else {
-          Brightness platformBrightness =
-              SchedulerBinding.instance.window.platformBrightness;
-          String defaultTheme = platformBrightness == Brightness.light
-              ? fixedValues.lightThemeId
-              : fixedValues.darkThemeId;
-
-          controller.setTheme(defaultTheme);
-          controller.forgetSavedTheme();
-        }
-      },
+      loadThemeOnInit: true,
+      defaultThemeId: SchedulerBinding.instance.window.platformBrightness ==
+              Brightness.light
+          ? fixedValues.lightThemeId
+          : fixedValues.darkThemeId,
       themes: [
         AppTheme(
           id: fixedValues.lightThemeId,
