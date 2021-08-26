bc8251ddbbea86d1fec91a91c6a053e09a501e2b
Makesh Vineeth <makeshvineeth1997@gmail.com>
2021-08-26 18:30:09 +0530

    Impprove infinite feed

--- lib/infinite/infinite_screen.dart
+++ lib/infinite/infinite_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:random_color/random_color.dart';
 import 'package:solid_color_fills/UI/colorItem.dart';
+import 'package:solid_color_fills/UI/scaleThis.dart';
 
 class InfiniteScreen extends StatefulWidget {
   const InfiniteScreen({Key key}) : super(key: key);
@@ -14,12 +15,6 @@ class _InfiniteScreenState extends State<InfiniteScreen> {
   final double spacing = 10.0;
 
   @override
-  void initState() {
-    super.initState();
-    _generateColors();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return OrientationBuilder(
       builder: (context, orientation) {
@@ -29,7 +24,6 @@ class _InfiniteScreenState extends State<InfiniteScreen> {
           padding: EdgeInsets.all(15.0),
           physics:
               AlwaysScrollableScrollPhysics(parent: BouncingScrollPhysics()),
-          cacheExtent: 2000,
           scrollDirection: Axis.vertical,
           addAutomaticKeepAlives: false,
           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
@@ -39,9 +33,15 @@ class _InfiniteScreenState extends State<InfiniteScreen> {
             crossAxisSpacing: spacing,
           ),
           itemBuilder: (context, index) {
-            if (index >= colorsList.length) _generateColors();
+            if (index >= colorsList.length - 1) _generateColors();
 
-            return ColorItem(mapEntry: colorsList.entries.elementAt(index));
+            return (index >= colorsList.length)
+                ? null
+                : ScaleThis(
+                    child: ColorItem(
+                      mapEntry: colorsList.entries.elementAt(index),
+                    ),
+                  );
           },
         );
       },
@@ -50,14 +50,18 @@ class _InfiniteScreenState extends State<InfiniteScreen> {
 
   void _generateColors() {
     RandomColor _randomColor = RandomColor();
-    List<Color> _color = _randomColor.randomColors(count: 10);
-    Map<String, dynamic> colors = {};
+    List<Color> _randomList = _randomColor.randomColors(count: 10);
 
-    for (var colorItem in _color) {
-      String colorName = getColorNameFromColor(colorItem).getName;
-      colors.addAll({colorName: colorItem});
+    if (_randomList != null && _randomList.isNotEmpty) {
+      Map<String, dynamic> colorsMapped = {};
+
+      for (Color colorItem in _randomList) {
+        String colorName = getColorNameFromColor(colorItem).getName;
+
+        if (colorName != null) colorsMapped.addAll({colorName: colorItem});
+      }
+
+      if (colorsMapped.isNotEmpty) colorsList.addAll(colorsMapped);
     }
-
-    colorsList.addAll(colors);
   }
 }
