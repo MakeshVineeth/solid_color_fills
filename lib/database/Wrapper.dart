252384d147e505b615c89d4ddffd6ea3f96e2f17
Makesh Vineeth <makeshvineeth1997@gmail.com>
2021-11-16 19:32:49 +0530

    Improve checking

--- lib/database/Wrapper.dart
+++ lib/database/Wrapper.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:solid_color_fills/screens/intro_screen.dart';
@@ -17,13 +19,20 @@ class _WrapperState extends State<Wrapper> {
 
   Future<void> initialTasks() async {
     try {
-      await FlutterDisplayMode.setHighRefreshRate();
-      SharedPreferences prefs = await SharedPreferences.getInstance();
-      bool showTutorial = prefs.getBool('first_launch') ?? true;
+      if (Platform.isAndroid) {
+        DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+        AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
+        int sdkVer = androidInfo.version.sdkInt;
 
-      setState(() => currentChild =
-          showTutorial ? IntroScreen(function: hideTutorial) : ScaffoldHome());
+        if (sdkVer >= 23) await FlutterDisplayMode.setHighRefreshRate();
+      }
     } catch (_) {}
+
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool showTutorial = prefs.getBool('first_launch') ?? true;
+
+    setState(() => currentChild =
+        showTutorial ? IntroScreen(function: hideTutorial) : ScaffoldHome());
   }
 
   @override
