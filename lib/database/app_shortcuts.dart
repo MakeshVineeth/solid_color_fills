559853e09ba8066667ccd4d4eb564422384da3f7
Makesh Vineeth <makeshvineeth1997@gmail.com>
2025-04-20 16:48:43 +0530

    Enhance build configuration and update app shortcuts with localized subtitles; remove unused drawable resources

--- lib/database/app_shortcuts.dart
+++ lib/database/app_shortcuts.dart
@@ -11,18 +11,21 @@ class AppShortcuts {
   static final collectionQuickAction = const ShortcutItem(
     type: 'action_collection',
     localizedTitle: FixedValues.collectionScreenTitle,
+    localizedSubtitle: 'Curated list of colors',
     icon: 'icon_collection',
   );
 
   static final colorsQuickAction = const ShortcutItem(
     type: 'action_colors',
     localizedTitle: FixedValues.colorPickersScreenTitle,
+    localizedSubtitle: 'Custom Color Pickers',
     icon: 'icon_colors',
   );
 
   static final feedQuickAction = const ShortcutItem(
     type: 'action_feed',
     localizedTitle: FixedValues.feedScreenTitle,
+    localizedSubtitle: 'Unlimited Color Feed',
     icon: 'icon_feed',
   );
 }
